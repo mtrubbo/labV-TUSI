26f4bce140226549180e25920afdc2dc85b6b034
Matias N. Trubbo <matiastrubbo@gmail.com>
2023-03-27 21:59:48 -0300

    reorganizar clase EntradaTeatro

--- TrabajoUno/src/main/java/com/trabajoUno/TiposEntrada/Teatro/EntradaTeatro.java
+++ TrabajoUno/src/main/java/com/trabajoUno/TiposEntrada/Teatro/EntradaTeatro.java
@@ -1,31 +1,22 @@
 package com.trabajoUno.TiposEntrada.Teatro;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Date;
 
 import com.trabajoUno.Entrada;
 import com.trabajoUno.Genero;
 
 public class EntradaTeatro extends Entrada {
-	
-	private final double costoEntrada = 1350.50;
+	private final float costoEntrada = 1350.50F;
 	
 	private List<Actor> actoresPrincipales;
-	private Genero genre;
+	private Genero genero;
 	
 	
 	public EntradaTeatro(String nombreShow, Date diaYHorario, int duracion, List<Actor> actoresPrincipales, Genero genre) {
-		super(nombreShow,diaYHorario,duracion);
-		
-		if(actoresPrincipales.size() <= 3) {			
-			this.actoresPrincipales = new ArrayList<Actor>();
-			this.actoresPrincipales = actoresPrincipales;
-		}
-		else {
-			this.actoresPrincipales = new ArrayList<Actor>();
-		}
-		this.genre = genre;
+			super(nombreShow,diaYHorario,duracion);
+		this.actoresPrincipales = actoresPrincipales;
+		this.genero = genre;
 	}
 
 
@@ -33,50 +24,24 @@ public class EntradaTeatro extends Entrada {
 		return actoresPrincipales;
 	}
 
+	public void setActoresPrincipales(List<Actor> actoresPrincipales) { this.actoresPrincipales = actoresPrincipales; }
 
-	public void setActoresPrincipales(List<Actor> actoresPrincipales) {
-		if(actoresPrincipales.size() <= 3) {
-			if(this.actoresPrincipales.size() != 0 ) {
-				if(!this.actoresPrincipales.equals(actoresPrincipales)) {					
-					this.actoresPrincipales = actoresPrincipales;
-				}
-			}
-			else {
-				this.actoresPrincipales = actoresPrincipales;
-			}
-		}
-	}
+	public Genero getGenero() { return genero; }
 
+	public void setGenero(Genero genero) { this.genero = genero; }
 
-	public Genero getGenre() {
-		return genre;
-	}
-
-
-	public void setGenre(Genero genre) {
-		this.genre = genre;
-	}
-
-
-	public double getCostoEntrada() {
-		return costoEntrada;
-	}
+	public double getCostoEntrada() { return costoEntrada; }
 
 
 	@Override
 	public String toString() {
-		return "Nombre Show: "+ this.nombreShow +" Duracion: "+ this.duracion + " Dia y Horario: "+ this.diaYHorario + " Costo: " + this.costoEntrada +  " actoresPrincipales=" + actoresPrincipales + ", genero: "
-				+ genre;
+		return "Nombre Show: "+ this.nombreShow +" Duracion: "+ this.duracion + " Dia y Horario: "+ this.diaYHorario
+				+ " Costo: " + this.costoEntrada +  ", actoresPrincipales=" + actoresPrincipales + ", genero: "
+				+ genero;
 	}
-
 
 	@Override
 	public float calcularCosto() {
-		// TODO Auto-generated method stub
-		return 0;
+		return costoEntrada;
 	}
-	
-	
-	
-	
 }
