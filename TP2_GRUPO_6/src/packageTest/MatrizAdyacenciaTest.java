44f8ce002f291626540b32525114bddaf9e60bc6
Gianluca Salvatori <salvatorigianluca@yahoo.com>
2023-04-11 11:45:35 -0300

    Update MatrizAdyacenciaTest.java

--- TP2_GRUPO_6/src/packageTest/MatrizAdyacenciaTest.java
+++ TP2_GRUPO_6/src/packageTest/MatrizAdyacenciaTest.java
@@ -2,6 +2,7 @@ package packageTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import frgp.utn.edu.ar.main.MatrizAdyacencia;
@@ -11,31 +12,50 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class MatrizAdyacenciaTest {
 
-	MatrizAdyacencia ma = new MatrizAdyacencia(5);
+	private MatrizAdyacencia ma = new MatrizAdyacencia(5);
 	
+	///A
+	@Disabled
 	@Test
 	public void agregarElementoTest() {
 		ma.agregarElemento(1, 1);
 		assertTrue(ma.existeElemento(1, 1));
 	}
 	
+	///B
+	@Disabled
 	@Test
 	public void agregarElementoSimetriaTest() {
-		ma.agregarElemento(2, 1);
-		assertEquals(ma.existeElemento(2, 1), ma.existeElemento(1, 2));
+		ma.agregarElemento(3, 2);
+		assertEquals(true, ma.existeElemento(2, 3));
 	}
 	
+	///C
+	@Disabled
 	@Test
 	public void eliminarElementoTest() {
+		ma.agregarElemento(1, 1);
 		ma.eliminarElemento(1, 1);
 		assertTrue(!ma.existeElemento(1, 1));
 		assertFalse(ma.existeElemento(1, 1));
 	}
-
+	
+	///D
+	@Disabled
+	@Test
+	public void eliminarElementoSimetricoTest() {
+		ma.agregarElemento(3, 2);
+		ma.eliminarElemento(3, 2);
+		assertEquals(false, ma.existeElemento(2, 3));
+	}
+	
 //	e. Crear un método llamado contarRelacionesTest que verifique
 //	que el método getCantidadRelaciones de la clase MatrizAdyacencia.
 //	Ejemplo: Si agregamos tres elementos [2,3] [1,4] y [1,2] ...
-//	hay un total de tres relaciones.
+//	hay un total de tres relaciones.}
+		
+	///E
+	@Disabled	
 	@Test
 	public void contarRelacionesTest() {
 		ma.agregarElemento(2, 3);
@@ -50,6 +70,9 @@ class MatrizAdyacenciaTest {
 //	f. Verificar que si se completan todos las posiciones de la matriz,
 //	todos estos elementos se hayan guardado correctamente
 //	en su posición original y en su simetrico.
+	
+	///F
+	@Disabled
 	@Test
 	public void existenTodosLosElementosTest(){
 		ma.agregarElemento(1,2);
