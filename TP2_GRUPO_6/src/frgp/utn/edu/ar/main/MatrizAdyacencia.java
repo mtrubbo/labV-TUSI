ed44ec9cf7b232dc392065f02b59450d63b9c699
Pablo Ezequiel Caero <pablo_ieme@hotmail.com>
2023-04-15 17:07:29 -0300

    Desarrollo de los puntos g, h, i

--- TP2_GRUPO_6/src/frgp/utn/edu/ar/main/MatrizAdyacencia.java
+++ TP2_GRUPO_6/src/frgp/utn/edu/ar/main/MatrizAdyacencia.java
@@ -3,41 +3,57 @@ package frgp.utn.edu.ar.main;
 public class MatrizAdyacencia {
 
 	private boolean[][] _ady;
-	private int cantElementos=0;
-	
-	public MatrizAdyacencia(int cant)
-	{
+	private int cantElementos = 0;
+
+	public MatrizAdyacencia(int cant) {
 		cantElementos = cant;
 		_ady = new boolean[cant][cant];
 	}
-	
-	public void agregarElemento(int i, int j)
-	{
+
+	public void agregarElemento(int i, int j) {
+
+		// ARREGLO PARA EXCEPCIONES PUNTO g.
+		if (i < 0 || i >= cantElementos) {
+			throw new IndexOutOfBoundsException("Índice de fila negativo: " + i);
+		}
+
+		// ARREGLO PARA EXCEPCIONES PUNTO h.
+		if (j < 0 || j >= cantElementos) {
+			throw new IndexOutOfBoundsException("Índice de columna negativo: " + j);
+		}
+
 		_ady[i][j] = true;
 		_ady[j][i] = true;
 	}
-	
-	public void eliminarElemento(int i, int j)
-	{
+
+	public void eliminarElemento(int i, int j) {
 		_ady[i][j] = false;
 		_ady[j][i] = false;
 	}
-	
-	public boolean existeElemento(int i, int j)
-	{
+
+	public boolean existeElemento(int i, int j) {
 		return _ady[i][j];
 	}
-	
-	public int getCantidadElementos()
-	{
-		int cont=0;
-		for(int i=0; i<=cantElementos-1; i++)
-		{
-		   for(int j=0; j<=cantElementos-1; j++)
-			   if(_ady[i][j]==true)
-				   cont++;
+
+	public int getCantidadElementos() {
+		int cont = 0;
+		for (int i = 0; i <= cantElementos - 1; i++) {
+			for (int j = 0; j <= cantElementos - 1; j++)
+				if (_ady[i][j] == true)
+					cont++;
 		}
-		cont = cont/2;
+		cont = cont / 2;
 		return cont;
+	}
+
+
+	public void agregarElementoFueraRango(int i, int j) {
+
+		// ARREGLO PARA EXCEPCIONES PUNTO i.
+		if (i < 0 || i >= cantElementos || j < 0 || j >= cantElementos) {
+			throw new IndexOutOfBoundsException("Índice de fila o columna fuera de rango: (" + i + ", " + j + ")");
+		}
+		
+		agregarElemento(i, j);
 	}
 }
--- TP2_GRUPO_6/src/packageTest/MatrizAdyacenciaTest.java
+++ TP2_GRUPO_6/src/packageTest/MatrizAdyacenciaTest.java
@@ -13,24 +13,24 @@ import org.junit.jupiter.params.provider.ValueSource;
 class MatrizAdyacenciaTest {
 
 	private MatrizAdyacencia ma = new MatrizAdyacencia(5);
-	
-	///A
+
+	/// A
 	@Disabled
 	@Test
 	public void agregarElementoTest() {
 		ma.agregarElemento(1, 1);
 		assertTrue(ma.existeElemento(1, 1));
 	}
-	
-	///B
+
+	/// B
 	@Disabled
 	@Test
 	public void agregarElementoSimetriaTest() {
 		ma.agregarElemento(3, 2);
 		assertEquals(true, ma.existeElemento(2, 3));
 	}
-	
-	///C
+
+	/// C
 	@Disabled
 	@Test
 	public void eliminarElementoTest() {
@@ -39,8 +39,8 @@ class MatrizAdyacenciaTest {
 		assertTrue(!ma.existeElemento(1, 1));
 		assertFalse(ma.existeElemento(1, 1));
 	}
-	
-	///D
+
+	/// D
 	@Disabled
 	@Test
 	public void eliminarElementoSimetricoTest() {
@@ -48,14 +48,14 @@ class MatrizAdyacenciaTest {
 		ma.eliminarElemento(3, 2);
 		assertEquals(false, ma.existeElemento(2, 3));
 	}
-	
-//	e. Crear un mÃ©todo llamado contarRelacionesTest que verifique
-//	que el mÃ©todo getCantidadRelaciones de la clase MatrizAdyacencia.
-//	Ejemplo: Si agregamos tres elementos [2,3] [1,4] y [1,2] ...
-//	hay un total de tres relaciones.}
-		
-	///E
-	@Disabled	
+
+	// e. Crear un mÃ©todo llamado contarRelacionesTest que verifique
+	// que el mÃ©todo getCantidadRelaciones de la clase MatrizAdyacencia.
+	// Ejemplo: Si agregamos tres elementos [2,3] [1,4] y [1,2] ...
+	// hay un total de tres relaciones.}
+
+	/// E
+	@Disabled
 	@Test
 	public void contarRelacionesTest() {
 		ma.agregarElemento(2, 3);
@@ -67,17 +67,17 @@ class MatrizAdyacenciaTest {
 		assertEquals(3, resultado);
 	}
 
-//	f. Verificar que si se completan todos las posiciones de la matriz,
-//	todos estos elementos se hayan guardado correctamente
-//	en su posiciÃ³n original y en su simetrico.
-	
-	///F
+	// f. Verificar que si se completan todos las posiciones de la matriz,
+	// todos estos elementos se hayan guardado correctamente
+	// en su posiciÃ³n original y en su simetrico.
+
+	/// F
 	@Disabled
 	@Test
-	public void existenTodosLosElementosTest(){
-		ma.agregarElemento(1,2);
-		ma.agregarElemento(0,3);
-		ma.agregarElemento(4,1);
+	public void existenTodosLosElementosTest() {
+		ma.agregarElemento(1, 2);
+		ma.agregarElemento(0, 3);
+		ma.agregarElemento(4, 1);
 
 		assertTrue(ma.existeElemento(1, 2));
 		assertTrue(ma.existeElemento(2, 1));
@@ -85,6 +85,60 @@ class MatrizAdyacenciaTest {
 		assertTrue(ma.existeElemento(3, 0));
 		assertTrue(ma.existeElemento(4, 1));
 		assertTrue(ma.existeElemento(1, 4));
+	}
+
+	
+	@Test
+	public void agregarElementoFilaNegativaTest() throws Exception {
+
+		int fila = -1; // fila negativa
+		int columna = 3; // columna válida
+
+		Exception excepcion = assertThrows(IndexOutOfBoundsException.class, () -> {
+			ma.agregarElemento(fila, columna); // llama al método agregarElemento con un índice de fila negativo
+														
+		});
+
+		String mensajeEsperado = "Índice de fila negativo: " + fila;
+		String mensajeObtenido = excepcion.getMessage(); // obtiene el mensaje de la excepción
+
+		assertEquals(mensajeEsperado, mensajeObtenido); // verifica que el mensaje de la excepción sea el esperado
+	}
+
+	
+	@Test
+	public void agregarElementoColumnaNegativaTest() {
+
+		    int fila = 2; // fila válida
+	        int columna = -1; // columna negativa
+	        
+	        Exception excepcion = assertThrows(IndexOutOfBoundsException.class, () -> {
+	            ma.agregarElemento(fila, columna); // llama al método agregarElemento con un índice de columna negativo
+	        });
+	        
+	        String mensajeEsperado = "Índice de columna negativo: " + columna;
+	        String mensajeObtenido = excepcion.getMessage(); // obtiene el mensaje de la excepción
+	        
+	        assertEquals(mensajeEsperado, mensajeObtenido); // verifica que el mensaje de la excepción sea el esperado
+
+	}
+
+	
+	@Test
+	public void agregarElementoFueraRangoTest()  {
+		
+		int fila = 1; // fila válida
+        int columna = 6; // columna fuera de rango
+        
+        Exception excepcion = assertThrows(IndexOutOfBoundsException.class, () -> {
+            ma.agregarElementoFueraRango(fila, columna); // llama al método agregarElemento con una columna fuera de rango
+        });
+        
+        String mensajeEsperado = "Índice de fila o columna fuera de rango: (" + fila + ", " + columna + ")";
+        String mensajeObtenido = excepcion.getMessage(); // obtiene el mensaje de la excepción
+        
+        assertEquals(mensajeEsperado, mensajeObtenido); // verifica que el mensaje de la excepción sea el esperado
+
 	}
 
 }
