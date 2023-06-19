1320de736eef48d0e52cfc76eaf2f66e0db807c4
Matias N. Trubbo <matiastrubbo@gmail.com>
2023-06-19 12:49:13 -0300

    [prog I] proyecto base con Spring Initializr

---
+++ progI-tpintegrador/src/test/java/frgp/utn/edu/ar/tpintegrador/TpIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package frgp.utn.edu.ar.tpintegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
