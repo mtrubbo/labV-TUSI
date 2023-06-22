93271070b16249a14191a5ac198e7d7bb1ef91d4
Gianluca Salvatori <salvatorigianluca@yahoo.com>
2023-06-22 17:03:18 -0300

    Create SecurityConfig.java

---
+++ progI-tpintegrador/src/main/java/frgp/utn/edu/ar/tpintegrador/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package frgp.utn.edu.ar.tpintegrador.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+// Configuración específica de Spring Security
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/public").permitAll()
+                .antMatchers("/private").authenticated()
+            .and()
+            .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/dashboard")
+                .permitAll()
+            .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Configuración de autenticación
+    }
+    
+/*  Paso 3: Configurar URLs y permisos
+    Aquí, al igual que antes, debes definir las reglas de autorización para diferentes URLs de 
+    tu aplicación en el método configure(HttpSecurity http) de la clase de configuración. Personaliza 
+    las reglas según tus necesidades. */
+
+/*  Paso 4: Configurar usuarios y roles
+    Al igual que antes, configura los usuarios y roles para la autenticación en el método 
+    configure(AuthenticationManagerBuilder auth) de la clase de configuración. */
+    
+/*  Paso 5: Integración con Spring MVC
+    En tu configuración de Spring MVC, asegúrate de incluir la configuración de Spring Security mediante
+    la anotación @Import(SecurityConfig.class). Esto asegurará que Spring MVC y Spring Security estén 
+    correctamente integrados. */
+}
