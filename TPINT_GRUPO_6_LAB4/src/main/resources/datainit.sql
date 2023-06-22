ccaa1ff20ade9fec436a51f5bd4dc6b154a25726
Matias N. Trubbo <matiastrubbo@gmail.com>
2023-06-21 21:22:17 -0300

    agregar roles y usuarios a datainit.sql

--- TPINT_GRUPO_6_LAB4/src/main/resources/datainit.sql
+++ TPINT_GRUPO_6_LAB4/src/main/resources/datainit.sql
@@ -1,5 +1,12 @@
 use tp_integrador
 
+insert into roles_usuario(descripcion) values ("contador");
+insert into roles_usuario(descripcion) values ("vendedor");
+insert into usuarios(contrasena, nombrePublico, nombreUsuario, id_rol)
+values ("contador", "Contador", "contador", 1);
+insert into usuarios(contrasena, nombrePublico, nombreUsuario, id_rol)
+values ("vendedor", "Vendedor", "vendedor", 2);
+
 insert into marcas(nombre)
 values('Nike')
 
